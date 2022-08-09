53ef7a7cce2e12b24948284cdb812f5ae1f154b1
CaioPotenza <caio@petize.com.br>
2022-08-09 16:46:08 -0300

    Feat: Initializing aplication;

---
+++ src/main/java/com/github/caio015/myonlineshop/MyonlineshopApplication.java
@@ -0,0 +1,13 @@
+package com.github.caio015.myonlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyonlineshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyonlineshopApplication.class, args);
+	}
+
+}
