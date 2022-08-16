e790286b154c3f8f2ad17d5e1056fdef45b69143
CaioPotenza <caio@petize.com.br>
2022-08-16 18:03:07 -0300

    Refactor: Correcting url;

--- src/main/java/com/github/caio015/myonlineshop/product/adapter/in/web/InsertProductController.java
+++ src/main/java/com/github/caio015/myonlineshop/product/adapter/in/web/InsertProductController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/admin")
+@RequestMapping(path = "/admin/product")
 @RequiredArgsConstructor
 public class InsertProductController {
 
